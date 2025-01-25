6489a04da89d2447dba0a6ee796c7b44cde92ae5
Kimyongari <kimybj123@naver.com>
2025-01-25 09:51:11 +0000

    [feat] add pipeline

--- src/evaluation/__init__.py
+++ src/evaluation/__init__.py
@@ -1,7 +1,6 @@
+# evaluation/__init__.py
+# 수정 전
 from .GPT_evaluation import G_evaluate
 from .retrieval_evaluation import retrieval_evaluate
 
-__all__ = [
-    "G_evaluate",
-    "retrieval_evaluate"
-]
+__all__ = ["G_evaluate", "retrieval_evaluate"]
