bf6f6f60d125a1c4c87491fbe4011b8c8cf8747a
Kimyongari <kimybj123@naver.com>
2025-01-31 22:25:47 +0900

    [docs] fix codes and add tutorial docs

--- Finbuddy/crews/__init__.py
+++ Finbuddy/crews/__init__.py
@@ -1,4 +1,4 @@
-from .blog_crew import BlogCrew
+from .news_search_crew import NewsCrew
 
-def Get_Blog_Crew():
-    return BlogCrew().blog_crew()
+def get_news_crew():
+    return NewsCrew().news_crew()
